a8741675e2418f5006e445c08af9b7a06112d367
aMonteSl <a.montesl.2020@alumnos.urjc.es>
2024-02-06 19:06:29 +0100

    CifradorAcabado

--- caesar.py
+++ caesar.py
@@ -18,14 +18,30 @@ A_ASCII = ord("A")
 Z_ASCII = ord("Z")
 MAX_KEY = Z_ASCII - A_ASCII + 1
 
+# Global constant to represent the minimum number of expected arguments
+MIN_ARGUMENTS = 2
+
 # Position of the key in sys.argv
 POS_DEFAULT_KEY_IN_ARGV = 1
+
+# Position of the .py name in sys.argv
+POS_DEFAULT_NAME_IN_ARGV = 0
 
 # Error message for incorrect program launch
 INPUT_ERROR = "Wrong way to launch the program."
 
 # Example of the correct way to launch the program
 EXAMPLE_LAUNCH = "Example: echo 'THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG' | ./caesar 23"
+
+
+def removeArgument(index):
+    # Delete arguments of argv
+    del sys.argv[index]
+
+
+def LengthArguments():
+    # Return the length of sys.argv
+    return len(sys.argv)
 
 
 def manageWrongInput():
@@ -46,17 +62,54 @@ def getKey():
     return key
 
 
-def getTextInput():
-    # Read text from standard input and convert it to uppercase
+def textInArguments():
+    # Check if there is text provided as command-line arguments.
+    return LengthArguments() > MIN_ARGUMENTS
+
+
+def getTextInArguments():
+    # Get text input provided as command-line arguments.
+    text = ''
+    for i in range(LengthArguments()):
+        word = sys.argv[i]
+        text += word
+        text += ' '
+    return text
+
+
+def getTextEstandarInput():
+    # Read text from standard input
     text_input = sys.stdin.read()
+
+    return text_input
+
+
+def removeTheNameAndKey():
+    # Remove first the key and then the name of the .py
+    removeArgument(POS_DEFAULT_KEY_IN_ARGV)
+    removeArgument(POS_DEFAULT_NAME_IN_ARGV)
+
+
+def getText():
+    # Get the text input for Caesar cipher encryption.
+    # This function retrieves the text input either from command-line arguments or standard input,
+    # depending on whether text is provided as arguments. The text is
+    # converted to uppercase for consistency.
+
+    if textInArguments():
+        removeTheNameAndKey()
+        text_input = getTextInArguments()
+    else:
+        text_input = getTextEstandarInput()
     text_input = text_input.upper()
+
     return text_input
 
 
 def controlInput():
     # Get key and text input for Caesar cipher
     key = getKey()
-    text_input = getTextInput()
+    text_input = getText()
     return key, text_input
 
 
@@ -106,8 +159,17 @@ def applyCaesar(key, text_input):
     return string_encryption
 
 
+def printEncrypted_text(text):
+    print(text)
+
+
+# Main program execution
 if __name__ == "__main__":
-    # Main program execution
+    # Get the key and text input for Caesar cipher
     key, text_input = controlInput()
+
+    # Apply Caesar cipher encryption to the input text using the obtained key
     encrypted_text = applyCaesar(key, text_input)
-    print(encrypted_text)
+
+    # Print the encrypted text
+    printEncrypted_text(encrypted_text)
