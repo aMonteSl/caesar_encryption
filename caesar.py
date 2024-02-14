bd56daa3a4e3fe6281e7fbcd37d62489f8b64a0b
adrian1 <a.montesl.2020@alumnos.urjc.es>
2024-02-14 11:14:53 +0100

    CaesarAcabado

--- caesar.py
+++ caesar.py
@@ -1,175 +1,44 @@
 #!/usr/bin/env python3
 
-"""
--------------------------------
-File: caesar.py
-Author: Adrián Montes Linares
-Date: 05/02/24
-Goal: The Caesar cipher encryption program takes a key and a text input, then applies the Caesar cipher algorithm to encrypt the text.
-      The goal is to shift each letter in the text by the specified key, ensuring that non-alphabetic characters remain unchanged.
-      The result is printed to the standard output.
--------------------------------
-"""
+from caesar_cipher_classes import CaesarCipher
 
-import sys
+class CaesarEncryption(CaesarCipher):
+    # Error message for incorrect program launch
+    INPUT_ERROR = "Wrong way to launch the program.\n"
+    # Example of the correct way to launch the program
+    EXAMPLE_STD_INPUT_LAUNCH = "Example1: echo 'THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG' | ./caesar 23\n"
+    # Another example of how to launch the program
+    EXAMPLE_ARGV_LAUNCH = "Example2: ./caesar 23 THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"
 
-# Constants for ASCII values of 'A' and 'Z', and maximum key value
-A_ASCII = ord("A")
-Z_ASCII = ord("Z")
-MAX_KEY = Z_ASCII - A_ASCII + 1
+    def __init__(self):
+        super().__init__()
+        self.key = None
+        self.white_text = None
+        self.cipher_text = None
 
-# Global constant to represent the minimum number of expected arguments
-MIN_ARGUMENTS = 2
+    def correctInput(self):
+        return self.key is not None and self.white_text is not None
 
-# Position of the key in sys.argv
-POS_DEFAULT_KEY_IN_ARGV = 1
+    def controlInput(self):
+        if not self.correctInput():
+            err_msg = self.INPUT_ERROR + self.EXAMPLE_STD_INPUT_LAUNCH + self.EXAMPLE_ARGV_LAUNCH
+            CaesarCipher.manageWrongInput(err_msg)
 
-# Position of the .py name in sys.argv
-POS_DEFAULT_NAME_IN_ARGV = 0
+    def getTextToEncrypt(self):
+        self.key, self.white_text = CaesarCipher.getInput()
+        self.controlInput()
 
-# Error message for incorrect program launch
-INPUT_ERROR = "Wrong way to launch the program."
+    def encrypt(self):
+        self.cipher_text = CaesarCipher.applyMovementKeyToString(self.white_text, self.key)
 
-# Example of the correct way to launch the program
-EXAMPLE_LAUNCH = "Example: echo 'THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG' | ./caesar 23"
+    def showCipherText(self):
+        print(self.cipher_text, end='')
 
+    def doEncryption(self):
+        self.getTextToEncrypt()
+        self.encrypt()
+        self.showCipherText()
 
-def removeArgument(index):
-    # Delete arguments of argv
-    del sys.argv[index]
-
-
-def LengthArguments():
-    # Return the length of sys.argv
-    return len(sys.argv)
-
-
-def manageWrongInput():
-    # If the input is incorrect, inform the user and exit the program
-    print(INPUT_ERROR + '\n' + EXAMPLE_LAUNCH)
-    exit()
-
-
-def getKey():
-    # Control wrong inputs and retrieve the key from command line arguments
-    try:
-        # Default argv[POS_DEFAULT_KEY_IN_ARGV] type is a string, change it to
-        # an int
-        key = int(sys.argv[POS_DEFAULT_KEY_IN_ARGV])
-    except (ValueError, IndexError):
-        # Handle ValueError and IndexError
-        manageWrongInput()
-    return key
-
-
-def textInArguments():
-    # Check if there is text provided as command-line arguments.
-    return LengthArguments() > MIN_ARGUMENTS
-
-
-def getTextInArguments():
-    # Get text input provided as command-line arguments.
-    text = ''
-    for i in range(LengthArguments()):
-        word = sys.argv[i]
-        text += word
-        text += ' '
-    return text
-
-
-def getTextEstandarInput():
-    # Read text from standard input
-    text_input = sys.stdin.read()
-
-    return text_input
-
-
-def removeTheNameAndKey():
-    # Remove first the key and then the name of the .py
-    removeArgument(POS_DEFAULT_KEY_IN_ARGV)
-    removeArgument(POS_DEFAULT_NAME_IN_ARGV)
-
-
-def getText():
-    # Get the text input for Caesar cipher encryption.
-    # This function retrieves the text input either from command-line arguments or standard input,
-    # depending on whether text is provided as arguments. The text is
-    # converted to uppercase for consistency.
-
-    if textInArguments():
-        removeTheNameAndKey()
-        text_input = getTextInArguments()
-    else:
-        text_input = getTextEstandarInput()
-    text_input = text_input.upper()
-
-    return text_input
-
-
-def controlInput():
-    # Get key and text input for Caesar cipher
-    key = getKey()
-    text_input = getText()
-    return key, text_input
-
-
-def getCharAscii(ascii_value):
-    # Get char of an Ascii_value
-    return chr(ascii_value)
-
-
-def getValueAscii(letter):
-    # Get the Ascii value of a char
-    return ord(letter)
-
-
-def isLetterAsciiValue(ascii_value):
-    # Rerturn if the ascci_value is between A-Z
-    return (A_ASCII <= ascii_value <= Z_ASCII)
-
-
-def addKeyToAsciiValue(ascii_value, key):
-    # Shift ASCII value by the key, considering the alphabet boundaries
-    return (ascii_value - A_ASCII + key) % MAX_KEY + A_ASCII
-
-
-def performCaesarCipherOperations(letter, key):
-    # Get the ASCII value of the letter to encrypt
-    ascii_value = getValueAscii(letter)
-
-    if isLetterAsciiValue(ascii_value):
-        # Perform Caesar cipher encryption
-        encrypted_ascii_value = addKeyToAsciiValue(ascii_value, key)
-    else:
-        # Non-alphabetic characters remain unchanged
-        encrypted_ascii_value = ascii_value
-
-    # Return the char corresponding to the encrypted_ascii_value
-    return getCharAscii(encrypted_ascii_value)
-
-
-def applyCaesar(key, text_input):
-    # Initialize string_encryption
-    string_encryption = ''
-
-    for letter in text_input:
-        # Encrypt each letter in the text using Caesar cipher
-        string_encryption += performCaesarCipherOperations(letter, key)
-
-    return string_encryption
-
-
-def printEncrypted_text(text):
-    print(text)
-
-
-# Main program execution
 if __name__ == "__main__":
-    # Get the key and text input for Caesar cipher
-    key, text_input = controlInput()
-
-    # Apply Caesar cipher encryption to the input text using the obtained key
-    encrypted_text = applyCaesar(key, text_input)
-
-    # Print the encrypted text
-    printEncrypted_text(encrypted_text)
+    cipher = CaesarEncryption()
+    cipher.doEncryption()
--- caesar_cipher_classes.py
+++ caesar_cipher_classes.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
 import sys
-import math
 import select
 
 class CaesarCipher:
+    # Constants for ASCII values of 'A' and 'Z', and maximum key value
+    A_ASCII = ord("A")
+    Z_ASCII = ord("Z")
+    MAX_KEY = Z_ASCII - A_ASCII + 1
+
     # Position of the key in sys.argv
     POS_DEFAULT_KEY_IN_ARGV = 1
 
@@ -16,6 +20,21 @@ class CaesarCipher:
 
     def __init__(self) -> None:
         pass
+
+    @staticmethod
+    def isLetterAsciiValue(ascii_value):
+        # Rerturn if the ascci_value is between A-Z
+        return (CaesarCipher.A_ASCII <= ascii_value <= CaesarCipher.Z_ASCII)
+
+    @staticmethod
+    def getValueAscii(letter):
+        # Get the Ascii value of a char
+        return ord(letter)
+    
+    @staticmethod
+    def getCharAscii(ascii_value):
+        # Get char of an Ascii_value
+        return chr(ascii_value)
     
     @staticmethod
     def manageWrongInput(err_msg):
@@ -55,24 +74,24 @@ class CaesarCipher:
 
     @staticmethod
     def textInArguments():
-        # Check if there is text provided as command-line arguments.
+        # Check if there is white_text provided as command-line arguments.
         return CaesarCipher.LengthArguments() > CaesarCipher.MIN_ARGUMENTS
     
     @staticmethod
     def getTextInArguments():
-        # Get text input provided as command-line arguments.
-        text = ''
+        # Get white_text input provided as command-line arguments.
+        white_text = ''
         for i in range(CaesarCipher.LengthArguments()):
             word = sys.argv[i]
-            text += word
-            text += ' '
-        return text
+            white_text += word
+            white_text += ' '
+        return white_text
     
     @staticmethod
     def getTextEstandarInput():
         # Check if there's something to read from standard input
         if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
-            # Read text from standard input
+            # Read white_text from standard input
             text_input = sys.stdin.read()
             return text_input
         else:
@@ -81,9 +100,9 @@ class CaesarCipher:
         
     @staticmethod
     def getText():
-        # Get the text input for Caesar cipher encryption.
-        # This function retrieves the text input either from command-line arguments or standard input,
-        # depending on whether text is provided as arguments. The text is
+        # Get the white_text input for Caesar cipher encryption.
+        # This function retrieves the white_text input either from command-line arguments or standard input,
+        # depending on whether white_text is provided as arguments. The white_text is
         # converted to uppercase for consistency.
         
         if CaesarCipher.textInArguments():
@@ -99,15 +118,47 @@ class CaesarCipher:
 
     @staticmethod
     def getInput():
-        # Get key and text input for Caesar cipher
+        # Get key and white_text input for Caesar cipher
         key = CaesarCipher.getKey()
         text_input = CaesarCipher.getText()
         return key, text_input
     
-# Input
-#------------------------------------------------------------------
-# Todo lo de cifrar o descifrar, la parte comun de sumar o restar a una string (apllyCaesar, esto cambiarlo por otro nmbre como aplicarMovimientoKeyString)
+# Todo calculo general de caesar
+#-----------------------------------------------------------------------------------
+    @staticmethod
+    def addKeyToAsciiValue(ascii_value, key):
+        # Shift ASCII value by the key, considering the alphabet boundaries
+        return (ascii_value - CaesarCipher.A_ASCII + key) % CaesarCipher.MAX_KEY + CaesarCipher.A_ASCII
 
+    @staticmethod
+    def performCaesarCipherOperations(letter, key):
+        # Get the ASCII value of the letter to encrypt
+        ascii_value = CaesarCipher.getValueAscii(letter)
+
+        if CaesarCipher.isLetterAsciiValue(ascii_value):
+            # Perform Caesar cipher encryption
+            encrypted_ascii_value = CaesarCipher.addKeyToAsciiValue(ascii_value, key)
+        else:
+            # Non-alphabetic characters remain unchanged
+            encrypted_ascii_value = ascii_value
+
+        # Return the char corresponding to the encrypted_ascii_value
+        return CaesarCipher.getCharAscii(encrypted_ascii_value)
+    
+    @staticmethod
+    def applyMovementKeyToString(white_text, key):
+        # Initialize string_encryption
+        string_encryption = ''
+
+        for letter in white_text:
+            # Encrypt each letter in the white_text using Caesar cipher
+            string_encryption += CaesarCipher.performCaesarCipherOperations(letter, key)
+
+        return string_encryption
+
+    
+
+"""
 class CaesarEncryption(CaesarCipher):
     # Error message for incorrect program launch
     INPUT_ERROR = "Wrong way to launch the program.\n"
@@ -118,28 +169,37 @@ class CaesarEncryption(CaesarCipher):
 
     def __init__(self):
         self.key = None
-        self.text = None
+        self.white_text = None
+        self.cipher_text = None
 
-    def controlInput(self, key, text):
-        if key and text:
-            self.key = key
-            self.text = text
-        else:
+    def correctInput(self):
+        return self.key and self.white_text
+
+    def controlInput(self):
+        if not CaesarEncryption.correctInput(self):
             err_msg = CaesarEncryption.INPUT_ERROR + CaesarEncryption.EXAMPLE_STD_INPUT_LAUNCH + CaesarEncryption.EXAMPLE_ARGV_LAUNCH
             CaesarCipher.manageWrongInput(err_msg)
-    
 
         
     def getTextToEncryp(self):
-        key, text = CaesarCipher.getInput()
-        CaesarEncryption.controlInput(self, key, text)
+        self.key, self.white_text = CaesarCipher.getInput()
+        CaesarEncryption.controlInput(self)
 
     
+    def encrypt(self):
+        self.cipher_text = CaesarCipher.applyMovementKeyToString(self.white_text, self.key)
 
+    def showCipherText(self):
+        print(f"{self.cipher_text}", end='')
+
+    def doEncryption(self):
+        CaesarEncryption.getTextToEncryp(self)
+        CaesarEncryption.encrypt(self)
+        CaesarEncryption.showCipherText(self)
+"""     
 
 
 if __name__ == "__main__":
-    cifrador = CaesarEncryption()
-    cifrador.getTextToEncryp()
-    print(cifrador.key)
-    print(cifrador.text)
+    #cifrador = CaesarEncryption()
+    #cifrador.doEncryption()
+    None
